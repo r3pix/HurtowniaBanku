43bec0535a9c07a6689c6dcb95e5433cf4eada97
Piotr <peterson2000@interia.pl>
2022-10-24 21:20:45 +0200

    Account Edits. data updates

--- HurtowniaBanku/Database/Tables/dbo/Account.sql
+++ HurtowniaBanku/Database/Tables/dbo/Account.sql
@@ -2,7 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [No] VARCHAR(50) NOT NULL, 
-    [Type] CHAR(1) NOT NULL, 
     [ClientId] INT NOT NULL, 
     [AccountTypeId] INT NOT NULL, 
     CONSTRAINT [FK_Account_Client] FOREIGN KEY ([ClientId]) REFERENCES [Client]([Id]),
